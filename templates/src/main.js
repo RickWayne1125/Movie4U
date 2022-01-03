49eb6df8531ec3ee3aa575131a1aea31f07b3c47
rickwayne1125 <1037457338@qq.com>
2022-01-03 22:56:13 +0800

    INIT check part

---
+++ templates/src/main.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el: '#checkList',
+    data: {
+        check_movies: [
+        {
+          name: 'M1',
+          tag: 'T1'
+        },
+        {
+          name: 'M2',
+          tag: 'T2'
+        },
+        {
+          name: 'M3',
+          tag: 'T3'
+        }
+      ]
+    }
+  });
