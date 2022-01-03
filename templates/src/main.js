fc40b7e8a4b9e33d9f164ef36a248863bd4f7cee
rickwayne1125 <1037457338@qq.com>
2022-01-04 00:00:47 +0800

    ADD Methods in JS

--- templates/src/main.js
+++ templates/src/main.js
@@ -1,19 +1,51 @@
+var url = "https://185.196.220.38";
 var app = new Vue({
-    el: '#checkList',
-    data: {
-        check_movies: [
-        {
-          name: 'M1',
-          tag: 'T1'
-        },
-        {
-          name: 'M2',
-          tag: 'T2'
-        },
-        {
-          name: 'M3',
-          tag: 'T3'
-        }
-      ]
-    }
-  });
+  el: "#checkList",
+  data: {
+    check_movies: [
+      {
+        id: 1,
+        name: "M1",
+        tag: "T1",
+      },
+      {
+        id: 2,
+        name: "M2",
+        tag: "T2",
+      },
+      {
+        id: 3,
+        name: "M3",
+        tag: "T3",
+      },
+    ],
+    pre_movies: [],
+  },
+  methods: {
+    refreshMovies() {
+      axios
+        .get(url + "/")
+        .then((response) => (this.check_movies = response.data.init_list))
+        .catch(function (error) {
+          // 请求失败处理
+          console.log(error);
+        });
+    },
+    addPreMovie(index) {
+      var id = check_movies[index].id;
+      this.pre_movies.push({ id: id });
+    },
+    getRecommend() {
+      axios
+        .post(url + "/recommend")
+        .then((response) => (this.rec_list = response.data.rec_list))
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+  mounted() {
+    // 获取冷启动电影
+    this.refreshMovies();
+  },
+});
