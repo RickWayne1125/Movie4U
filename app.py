2edda8edd72a8ddadde70484919805c6c2848bda
1013634335@qq.com <1013634335@qq.com>
2022-01-04 18:33:34 +0800

    init_list格式更改

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, request,jsonify
+from flask import Flask, jsonify
+from flask import make_response
+from flask_cors import CORS
 
 from pop_movies_show.top_movie_on_tag import get_check_list
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
+app.config.update(
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='Lax',
+)
 
 
 # 返回电影信息，title，poster，tag
@@ -11,11 +19,13 @@ def movie_show():
     pass
 
 
-@app.route('/')
+@app.route('/init')
 def init_pop_movies():
     init_list = get_check_list()
+    resp = make_response('Test')
+    resp.set_cookie('same-site-cookie', 'strict', samesite='Lax')
+    # return resp
     return jsonify(init_list)
-    # return render_template('index.html')
 
 
 if __name__ == '__main__':
