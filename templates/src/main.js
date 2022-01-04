9c52dbea00d5af6aae1fd46bfcedca4a0d8a416e
rickwayne1125 <1037457338@qq.com>
2022-01-04 13:51:01 +0800

    ADD Basic View

--- templates/src/main.js
+++ templates/src/main.js
@@ -3,21 +3,15 @@ var app = new Vue({
   el: "#checkList",
   data: {
     check_movies: [
-      {
-        id: 1,
-        name: "M1",
-        tag: "T1",
-      },
-      {
-        id: 2,
-        name: "M2",
-        tag: "T2",
-      },
-      {
-        id: 3,
-        name: "M3",
-        tag: "T3",
-      },
+      // 用于布局测试
+      { id: 1, name: "M1", tag: "T1" },
+      { id: 2, name: "M2", tag: "T2" },
+      { id: 3, name: "M3", tag: "T3" },
+      { id: 4, name: "M4", tag: "T4" },
+      { id: 5, name: "M5", tag: "T5" },
+      { id: 6, name: "M6", tag: "T6" },
+      { id: 7, name: "M7", tag: "T7" },
+      { id: 8, name: "M8", tag: "T8" },
     ],
     pre_movies: [],
   },
