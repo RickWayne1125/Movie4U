450a688e648e2686dfb07c1b5297c059e1d44b47
1013634335@qq.com <1013634335@qq.com>
2022-01-04 12:25:14 +0800

    init_list格式更改

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from random import randrange
-from flask import Flask
-from pop_movies_show.top_movie_on_tag import get_movie_array_on_tag
+from flask import Flask, render_template, request,jsonify
+
+from pop_movies_show.top_movie_on_tag import get_check_list
 
 app = Flask(__name__)
-tag_num_max = 1126
+
 
 # 返回电影信息，title，poster，tag
 @app.route('/movie')
@@ -13,20 +13,9 @@ def movie_show():
 
 @app.route('/')
 def init_pop_movies():
-    # 从tag对应电影集合初始取8个movie，每个tag最多对应两个
-    movie_id_set = set()
-    max_len = 8
-    movie_cnt = 2
-    tag_id = randrange(tag_num_max)
-    while len(movie_id_set) != max_len:
-        tmp_array, tmp_cnt = get_movie_array_on_tag(tag_id, movie_cnt)
-        for mid in tmp_array:
-            movie_id_set.add(mid)
-        movie_cnt = min(max_len - len(movie_id_set), 2)
-        tag_id = (tag_id + 1) % tag_num_max
-    print(tag_id)
-    print(movie_id_set)
-    return 'Hello World!'
+    init_list = get_check_list()
+    return jsonify(init_list)
+    # return render_template('index.html')
 
 
 if __name__ == '__main__':
