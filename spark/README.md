9497debc9145059fbd9f495bac68e0da6e68fefc
Chufan Chen <50797312+RetownPlato@users.noreply.github.com>
2022-01-04 18:26:07 +0800

    add spark version of recommendation.py

--- spark/README.md
+++ spark/README.md
@@ -4,4 +4,8 @@ mongoimport -d MovieLens  --type csv --file ratings.csv --headerline
 mongoimport -d MovieLens  --type csv --file movies.csv --headerline
 ```
 2. engine.py
-Recommendation engine using ALS
+Recommendation engine using Collaborative Filtering-Alternating Least Square
+3. sparkfm.py
+Recommendation engine using Factorization Machines
+4. recommendation.ipynb
+Adoption of `../recommendation/recommendation.py` for Spark
---
+++ spark/sparkfm.py
@@ -0,0 +1,43 @@
+import os
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class RecommendationEngine:
+    """A movie recommendation engine
+    """
+
+    def __init__(self, sc, dataset_path):
+        """Init the recommendation engine given a Spark context and a dataset path
+        """
+
+        logger.info("Starting up the Recommendation Engine: ")
+
+        self.sc = sc
+
+        # Load ratings data for later use
+        logger.info("Loading Ratings data...")
+        ratings_file_path = os.path.join(dataset_path, 'ratings.csv')
+        ratings_raw_RDD = self.sc.textFile(ratings_file_path)
+        ratings_raw_data_header = ratings_raw_RDD.take(1)[0]
+        self.ratings_RDD = ratings_raw_RDD.filter(lambda line: line!=ratings_raw_data_header)\
+            .map(lambda line: line.split(",")).map(lambda tokens: (int(tokens[0]),int(tokens[1]),float(tokens[2]))).cache()
+        # Load movies data for later use
+        logger.info("Loading Movies data...")
+        movies_file_path = os.path.join(dataset_path, 'movies.csv')
+        movies_raw_RDD = self.sc.textFile(movies_file_path)
+        movies_raw_data_header = movies_raw_RDD.take(1)[0]
+        self.movies_RDD = movies_raw_RDD.filter(lambda line: line!=movies_raw_data_header)\
+            .map(lambda line: line.split(",")).map(lambda tokens: (int(tokens[0]),tokens[1],tokens[2])).cache()
+        self.movies_titles_RDD = self.movies_RDD.map(lambda x: (int(x[0]),x[1])).cache()
+        # Pre-calculate movies ratings counts
+        self.__count_and_average_ratings()
+
+        # Train the model
+        self.rank = 8
+        self.seed = 5
+        self.iterations = 10
+        self.regularization_parameter = 0.1
+        self.__train_model() 
