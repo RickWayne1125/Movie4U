3daa8f87533447ed61c62979450cbe5e92c6bd42
1013634335@qq.com <1013634335@qq.com>
2022-01-04 23:38:01 +0800

    查找时间优化

--- pop_movies_show/top_movie_on_tag.py
+++ pop_movies_show/top_movie_on_tag.py
@@ -5,14 +5,17 @@ import os.path
 import time
 
 tag_num_max = 1126
-db_uri = "mongodb://mongodb.chufanchen.com:27017"  # localhost 
+db_uri = "mongodb.chufanchen.com:27017"  # mongodb.chufanchen.com:27017
 movie_info_db_name = "MovieLens"  # 本地:test，远程:MovieLens
 movie_info_col_name = "movies"
 tag_info_col_name = "genome-tags"
 tag_movie_array_col_name = "tagId-movieIdArray"
+hot_movie_on_single_tag_col_name = "hot_movie_on_single_tag"
 client = pymongo.MongoClient(db_uri)
 root_db = client[movie_info_db_name]
-pic_folder_path = "D:/PycharmProjects/Movie4U/pic"      # 图片文件夹路径
+pic_folder_path = "/home/cqu/mlp-20m/MLP-20M"  # 图片文件夹路径
+skip_len = 0
+LIMIT_LEN = 8
 
 
 # 根据movieId返回title
@@ -48,56 +51,34 @@ def get_movie_array_on_tag(tag_id, count=2):
         print("error")
 
 
-# 从tag对应电影集合初始取8个movie，每个tag最多对应两个
+# 选取8部热门电影，{'_id': 电影id, 'tag_id': 标签id, 'tag_name': '标签名', 'title': '电影名'}
 def get_check_list():
-    check_list = []
-    movie_id_set = set()
-    max_len = 8
-    movie_cnt = 2
-    # tag_id = 1
-    tag_id = randrange(tag_num_max)
-    # print("randomId",tag_id)
-    time0=time.time()
-    while len(check_list) != max_len:
-        tag_name = get_tag_info(tag_id)
-        tmp_array, tmp_cnt = get_movie_array_on_tag(tag_id, movie_cnt)
-        for m_id in tmp_array:
-            if m_id in movie_id_set:  # movie_id去重
-                continue
-            else:
-                movie_id_set.add(m_id)
-            list_item = dict()
-            list_item.update({'id': m_id})
-            list_item.update({'name': get_movie_info(m_id)})
-            list_item.update({'tag': tag_name})
-            m_path = "{}/{}.svg".format(pic_folder_path, m_id)
-            if Path(m_path).is_file():
-                list_item.update({'path_exist': 1})
-                list_item.update({'path': m_path})
-            else:
-                list_item.update({'path_exist': 0})
-                list_item.update({'path': m_path})
-            check_list.append(list_item)
-        movie_cnt = min(max_len - len(check_list), 2)
-        tag_id = (tag_id + 1) % tag_num_max
-    print(time.time()-time0)
+    col = root_db[hot_movie_on_single_tag_col_name]
+    time0 = time.time()
+    second = time.localtime().tm_sec % 46  # 数据集369，最多46组
+    print("second:", second)
+    hot_movie_list = col.find().skip(second * LIMIT_LEN).limit(LIMIT_LEN)
+    check_list = [i for i in hot_movie_list]
+    print("cost: ", time.time() - time0)
     return check_list
 
 
 if __name__ == '__main__':
-    # movie信息获取 测试
-    print(get_movie_info(1))
-    # tag信息获取 测试
-    print(get_tag_info(760))
+    # # movie信息获取 测试
+    # print(get_movie_info(1))
+    # # tag信息获取 测试
+    # print(get_tag_info(760))
 
-    # 取tag对应movieId 测试
-    tag_zero_array = get_movie_array_on_tag(tag_id=2, count=2)
-    tag_one_array = get_movie_array_on_tag(tag_id=6, count=2)
-    tag_multi_array = get_movie_array_on_tag(tag_id=3, count=2)
-    print(tag_zero_array)
-    print(tag_one_array)
-    print(tag_multi_array)
+    # # 取tag对应movieId 测试
+    # tag_zero_array = get_movie_array_on_tag(tag_id=2, count=2)
+    # tag_one_array = get_movie_array_on_tag(tag_id=6, count=2)
+    # tag_multi_array = get_movie_array_on_tag(tag_id=3, count=2)
+    # print(tag_zero_array)
+    # print(tag_one_array)
+    # print(tag_multi_array)
 
     # get_check_list 测试
-    for item in get_check_list():
-        print(item)
+    for i in range(5):
+        time.sleep(1)
+        for item in get_check_list():
+            print(item)
