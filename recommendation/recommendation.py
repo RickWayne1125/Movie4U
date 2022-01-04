b6f097e3dc2cdeb6a58bab3024009a48fe01da8a
ce <1359386000@qq.com>
2022-01-04 22:37:35 +0800

    修改输入参数为用户选择电影movieId列表

--- recommendation/recommendation.py
+++ recommendation/recommendation.py
@@ -26,6 +26,28 @@ def recommendationForUser(thisUserId):
     return recommendationResult
 
 
+# 输入为用户选择的感兴趣电影的movieId列表
+# 返回为该用户推荐的10个电影的movieId
+def recommendationForUser_2(moviesList):
+    myClient = pymongo.MongoClient("MongoDB_uri")
+    myDb = myClient["MovieLens"]
+    tableSimilarities = myDb["similarities"]
+    interests = dict()
+
+    for movieID in moviesList:
+        for recordSimilarity in tableSimilarities.find({'movieId_1': movieID}):
+            movieId_2 = recordSimilarity['movieId_2']
+            interests.setdefault(movieId_2, 0)
+            interests[movieId_2] += recordSimilarity['value']
+
+    recommendationResult = []
+    for i in range(10):
+        thisMovieId = max(interests, key=interests.get)
+        recommendationResult.append(thisMovieId)
+        del interests[thisMovieId]
+
+    return recommendationResult
+
 
 
 
